680106ccbb6267769233fb545a37a6885d5113e4
aferjault <ferjaultarthur@gmail.com>
2023-03-19 16:33:44 +0100

    Ajoutez des fichiers projet. modif

---
+++ MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Battleship_project
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ClosePopupClicked(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            frame.Navigate(typeof(RulesPage));
+
+        }
+
+        private void Ranking_Click(object sender, RoutedEventArgs e)
+        {
+            frame.Navigate(typeof(RankingPage));
+
+        }
+    }
+}
---
+++ RulesPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Battleship_project
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class RulesPage : Page
+    {
+        public RulesPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Retour_Click(object sender, RoutedEventArgs e)
+        {
+            frame.Navigate(typeof(MainPage));
+        }
+
+        private void HandleCheck(object sender, RoutedEventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            if (Facile.IsChecked == true || Intermediaire.IsChecked == true || Difficile.IsChecked == true) frame.Navigate(typeof(GamePage)); ;
+        }
+    }
+}
